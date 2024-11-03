622640b0e7f64e20c9ff674d3f562c77aa945527
nestor711 <nestor.heredia@correounivalle.edu.co>
2024-11-03 16:03:47 -0500

    Squashed commit of the following:
    commit 34db314b8f2c0996be12e2b73154fcd9a320429f
    Merge: e862301 f40bfa7
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Sun Nov 3 15:59:32 2024 -0500
    
        Merge remote-tracking branch 'origin/Develop' into ndhg
    
    commit e8623015aad8855036bd139dfedb0b839538cf23
    Merge: 4c983a4 32ec4b9
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 21:49:08 2024 -0500
    
        Merge branch 'main' into ndhg
    
    commit 4c983a4297b4397e7b619d5e25ac8f5059b5fb68
    Merge: e6f1cad 6e1e1a6
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:29:11 2024 -0500
    
        Merge branch 'main' into ndhg
    
    commit e6f1cad66a8c4f5feb6a18c5a9f3db16e2e316a2
    Merge: 5b75562 dd72cb0
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:24:48 2024 -0500
    
        Merge branch 'main' into ndhg
    
    commit 5b7556209cf63c16812bad9b69a2ff7313c97161
    Merge: 97de96b 0dbafa1
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:24:05 2024 -0500
    
        Merge branch 'ndhg' of https://github.com/Carlos-39/Proyecto-Integrador into ndhg
    
    commit 97de96b4b3f75b83cd06468bd5165d27eb93bf44
    Merge: da5c3df b212724
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:23:28 2024 -0500
    
        Merge branch 'main' into ndhg
    
    commit 0dbafa11b46e933304a69c38dd0f5a414abcfea0
    Merge: da5c3df b212724
    Author: Néstor David Heredia Gutierrez <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:21:31 2024 -0500
    
        Merge branch 'main' into ndhg
    
    commit da5c3dfb7e5d27ad0fd80852060e5537fbe4daa0
    Merge: dc8021a e81a144
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:18:13 2024 -0500
    
        Merge branch 'ndhg' of https://github.com/Carlos-39/Proyecto-Integrador into ndhg
    
    commit dc8021a66f96e17e149c3b9684a6311ea50161a5
    Merge: 7c2b573 516f1c1
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:17:18 2024 -0500
    
        Merge branch 'main' into ndhg
    
    commit e81a144c0ef266ff9ebe6bdc55bc52a2a03297a2
    Merge: 7c2b573 516f1c1
    Author: Néstor David Heredia Gutierrez <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:15:13 2024 -0500
    
        Merge branch 'main' into ndhg
    
    commit 7c2b5730fb90fbe0c331bade335863db21daba7d
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:12:26 2024 -0500
    
        Update acidification.jsx
    
    commit 0f3a462a0acc2c4e3baa60800af9186604934d15
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 15:11:16 2024 -0500
    
        Enhance lighting and shadow effects in Acidification component
    
        - Added ambient light for overall illumination.
        - Configured directional light with shadows for realistic depth.
        - Enabled shadows on bubbles and particles for a dynamic look.
        - Adjusted light positions and intensities for improved visual appeal.
    
    commit 4bed85be02db81441de244e43a03bf69bba3d395
    Author: nestor711 <nestor.heredia@correounivalle.edu.co>
    Date:   Tue Oct 29 08:11:51 2024 -0500
    
        Acidification and Escene updated

---
+++ src/pages/ocean-acidification/AcidificationPage.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { FaTrophy } from "react-icons/fa";
+import useAuthStore from "../../stores/use-auth-store";
+import "./AcidificationPage.css";
+import projectLogo from "../../assets/images/logo.png";
+import acerca from "../../assets/images/Acerca_del_tema_acidificacion.png";
+import quiz from "../../assets/images/Quiz_Acidificacion.jpg";
+
+const AcidificationPage = () => {
+  const { user, observeAuthState } = useAuthStore();
+  
+  React.useEffect(() => {
+    observeAuthState();
+  }, [observeAuthState]);
+
+  const capitalizeWords = (text) => {
+    return text
+      .toLowerCase()
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+
+  const username = user ? capitalizeWords(user.displayName) : "Invitado";
+
+  return (
+    <div className="acidification-page">
+      <header className="header">
+        <div className="logo-container">
+          <img
+            src={projectLogo}
+            alt="BlueSphere Studios Logo"
+            className="logo"
+          />
+          <div className="company-info">
+            <h1 className="title">BlueSphere Studios</h1>
+          </div>
+        </div>
+        <div className="user-info">
+          <span className="username">Hola, {username}</span>
+          <div className="trophy-container">
+            <FaTrophy className="trophy-icon" />
+            <span>Trofeos</span>
+          </div>
+        </div>
+      </header>
+
+      <main className="main-content">
+        <h2 className="page-title">Acidificación de los Océanos</h2>
+        
+        <div className="description-box">
+          <p className="description-text">
+            La acidificación de los océanos es el resultado de la absorción de dióxido de carbono
+            (CO₂) de la atmósfera, lo que aumenta el ácido carbónico y reduce el pH del agua. Desde
+            la Revolución Industrial, los océanos han absorbido alrededor del 30% del CO₂ emitido,
+            disminuyendo el pH en aproximadamente 0.1 unidades. Esto perjudica a organismos
+            marinos como corales y moluscos, amenazando ecosistemas y comunidades costeras.
+          </p>
+        </div>
+
+        <div className="sections-container">
+          <div className="section-card">
+            <h3 className="section-title">Acerca del tema</h3>
+            <img 
+              src={acerca}
+              alt="Diagrama de acidificación"
+              className="section-image"
+            />
+            <button className="button learn-more-btn">
+              Aprender más
+            </button>
+          </div>
+
+          <div className="section-card">
+            <h3 className="section-title">Probarme</h3>
+            <img 
+              src={quiz}
+              alt="Quiz imagen"
+              className="section-image"
+            />
+            <button className="button quiz-btn">
+              Comenzar quiz
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default AcidificationPage;
